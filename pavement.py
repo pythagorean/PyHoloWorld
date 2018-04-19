2525ed47f36e779b3598ae69af055119201dd036
Michael Goldman <mike.goldman@allstreets.org>
2018-04-19 03:56:17 -0400

    Construct javascript runtime only once per build even if more modules existed

--- pavement.py
+++ pavement.py
@@ -4,28 +4,34 @@ from paver.easy import *
 def build():
     sh('transcrypt -n ui/*.py')
     sh('transcrypt -n -p .none dna/*/*.py')
+
     print('Modifying javascript for otto.')
     startswith = path('node_modules/string.prototype.startswith/startswith.js').lines()
+    runtime = None
+
     for dir in path('dna').dirs():
-        for infile in path(dir + '/__javascript__').files('*.mod.js'):
-            inlines = []
-            for line in infile.lines()[2:]:
+        for modfile in path(dir + '/__javascript__').files('*.mod.js'):
+            modlines = []
+            for line in modfile.lines()[2:]:
                 if line[3] == '_': break
-                inlines.append(line[2:])
-            runtime = startswith
-            runtime.append("\n// Transcrypt runtime code for otto: no bytearray support")
-            jsfile = path(infile.relpath()[:-7] + '.js')
-            strip = False
-            for line in jsfile.lines()[3:]:
-                if line[1:2] == '(': break
-                if line[4:22] == "function bytearray":
-                    strip = True
-                if strip and line[4:16] == "function str":
-                    strip = False
-                if not strip:
-                    runtime.append(line)
-            runtime.append('\n// Transcrypted Python module for otto')
-            jsfile.write_lines(runtime + inlines)
+                modlines.append(line[2:])
+
+            jsfile = path(modfile.relpath()[:-7] + '.js')
+            if not runtime: # Construct javascript runtime for otto
+                runtime = startswith[:] # required library
+                runtime.append("\n// Transcrypt runtime code for otto: no bytearray support")
+                strip = False
+                for line in jsfile.lines()[3:]:
+                    if line[1:2] == '(': break
+                    if line[4:22] == "function bytearray":
+                        strip = True
+                    if strip and line[4:16] == "function str":
+                        strip = False
+                    if not strip:
+                        runtime.append(line)
+                runtime.append('\n// Transcrypted Python module for otto')
+
+            jsfile.write_lines(runtime + modlines)
 
 @task
 def clean():
