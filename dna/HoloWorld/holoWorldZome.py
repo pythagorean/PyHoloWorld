a575d6526a0cb65a0335091a5d17d2637c936a3b
Michael Goldman <mike.goldman@allstreets.org>
2018-04-20 06:30:47 -0400

    Added comments to holoWorldZome.py matching those in HoloWorld's js

--- dna/HoloWorld/holoWorldZome.py
+++ dna/HoloWorld/holoWorldZome.py
@@ -1,20 +1,36 @@
 __pragma__ ('noalias', 'get') # Defaults to py_get, js_get to get
 
+# EXPOSED METHODS
+
+# creates a holoWorldEntry entry
 def holoWorldEntryCreate(entry):
     return commit('holoWorldEntry', entry)
 
+# retrieves a holoWorldEntry entry
 def holoWorldEntryRead(hash):
     return get(hash)
 
+ # Called only when your source chain is generated
+ # return {boolean} success
 def genesis():
     return True
 
+#-----------------------------------------------------------------
+# validation functions for every DHT entry change
+#-----------------------------------------------------------------
+
 def validateCommit(entryName, entry, header, pkg, sources):
-    if entryName == 'holoWorldEntry': return True
+    if entryName == 'holoWorldEntry':
+        # in order for the 'commit' action to work, validateCommit (given a holoWorldEntry) must return True
+        # there is no special validation that we have to perform for our simple app
+        return True
+    # default: invalid entry name
     return False
 
 def validatePut(entryName, entry, header, pkg, sources):
-    if entryName == 'holoWorldEntry': return True
+    if entryName == 'holoWorldEntry':
+        return True
+    # default: invalid entry name
     return False
 
 def validatePutPkg(entryName):
